a69ddcd64fa849fbcd6905440e256b0494c38aa0
stixlee <michael.lee.1983@iclllud.com>
2024-01-10 11:44:14 -0700

    Initiali Commit ]

---
+++ ViewModelPatterns/HomeView.swift
@@ -0,0 +1,49 @@
+//
+//  HomeView.swift
+//  ViewModelPatterns
+//
+//  Created by Michael Lee on 1/10/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    let sideMenuWidth = 100.0
+    
+    @State var showSideView: Bool = false
+    
+    var body: some View {
+        ZStack {
+            TabView {
+                
+                PlaceholderView()
+                    .tabItem {
+                        Label("Shared", systemImage: "house.fill")
+                    }
+                PlaceholderView()
+                    .tabItem {
+                        Label("Aggregate", systemImage: "bitcoinsign.circle.fill")
+                    }
+                PlaceholderView()
+                    .tabItem {
+                        Label("Composite", systemImage: "sunglasses.fill")
+                    }
+                PlaceholderView()
+                    .tabItem {
+                        Label("Isolated", systemImage: "graduationcap.fill")
+                    }
+                PlaceholderView()
+                    .tabItem {
+                        Label("Binding", systemImage: "person.fill")
+                    }
+            }
+            .background(Color.primary)
+            .accentColor(.orange)
+        }
+
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ ViewModelPatterns/PlaceholderView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceholderVIew.swift
+//  ViewModelPatterns
+//
+//  Created by Michael Lee on 1/10/24.
+//
+
+import SwiftUI
+
+struct PlaceholderView: View {
+    var body: some View {
+        Text("Placeholder")
+    }
+}
+
+#Preview {
+    PlaceholderView()
+}
