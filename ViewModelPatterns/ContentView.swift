d0e0a61e37adf1bf10ba6be569ae566207d6b2dd
stixlee <michael.lee.1983@iclllud.com>
2024-01-10 11:26:17 -0700

    Initial Commit

---
+++ ViewModelPatterns/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViewModelPatterns
+//
+//  Created by Michael Lee on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViewModelPatterns/ViewModelPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModelPatternsApp.swift
+//  ViewModelPatterns
+//
+//  Created by Michael Lee on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewModelPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
